40d4d3bd1ffacba1450bfa0472fe65c87c65af7a
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-04-18 01:26:06 +0700

    Add simple class definition example

---
+++ the-basics/simple-class-definition.php
@@ -0,0 +1,24 @@
+<?php
+/******************************************************************************
+1. Class definition
+   "class [ClassName] {}"
+
+2. Valid class name
+   Starts with a letter or underscore, followed by any number of letters,
+   numbers, or underscores
+
+3. A Class may contain its own consts, vars (properties), functions (methods)
+******************************************************************************/
+
+class SimpleClass
+{
+    // Property declaration
+    public $var = 'a default value';
+
+    // Method declaration
+    public function displayVar()
+    {
+        echo $this->var;
+    }
+}
+?>
