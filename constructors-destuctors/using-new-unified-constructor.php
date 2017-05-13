61bfb4cfcdff6ec844a8cd0082f10bda7bf46ae4
Wanda Ichsanul Isra <wandaisra@gmail.com>
2017-05-13 20:04:16 +0700

    Add using new unified constructor example

---
+++ constructors-destuctors/using-new-unified-constructor.php
@@ -0,0 +1,40 @@
+<?php
+/******************************************************************************
+Points:
+    1. Constructor method called on each newly-created object.
+
+    2. Constructor method structure:
+       void __construct ([ mixed $args = "" [, $... ]] )
+
+    3. We can used this method to initialize anything that the object may need
+       before it is used.
+
+Output:
+    In BaseClass constructor
+    In BaseClass constructor
+    In SubClass constructor
+    In BaseClass constructor
+******************************************************************************/
+
+class BaseClass {
+    function __construct() {
+        print "In BaseClass constructor\n";
+    }
+}
+
+class SubClass extends BaseClass {
+    function __construct() {
+        parent::__construct();
+        print "In SubClass constructor\n";
+    }
+}
+
+class OtherSubClass extends BaseClass {
+}
+
+$obj = new BaseClass();
+
+$obj = new SubClass();
+
+$obj = new OtherSubClass();
+
